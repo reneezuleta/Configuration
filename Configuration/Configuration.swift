717fcbac63f67890f014460c8baf330e46f0bedd
reneez <renee.zuleta@gmail.com>
2021-06-01 09:26:04 -0400

    rythm changes

--- Configuration/Configuration.swift
+++ Configuration/Configuration.swift
@@ -8,6 +8,7 @@
 import Foundation
 import utilities
 
+
 public enum TuneType{  // used to know what was the type of the last tune played
     case melody    // used to createtune
     case beat    // used to createtune
@@ -43,7 +44,6 @@ public struct Config{
     public var sigNum:UInt8
     public var sigDen:UInt8
     public var signatureEventInfo:[UInt8]
-    public var bpmString:String
     public var bpm:UInt16
     public var resolutionString:String
     public var resolution:Double
@@ -51,11 +51,16 @@ public struct Config{
     public var microSecPerTick:UInt32
     public var measureCountString:String
     public var measureCount:UInt8
-    public var loopForEverString:String
+    //public var loopForEverString:String
     public var loop:Bool
     public var rithmPattern:UInt32
-    public var rithmInstruments: [UInt8] // will default to 32 to allow max resolution
-    public var midiParams: midiDefines
+    public var preloadedGrooves :[UInt32]
+    public let defaultInstrument1 :(UInt8,String) = (UInt8(42),"Closed Hi Hat")
+    public let defaultInstrument2 :(UInt8,String) = (UInt8(38),"Acustic Snare")
+    public let defaultRandomBeatInstr : (UInt8,String) = (UInt8(36),"Bass Drum 1")
+    public let defaultStartMarkerInstr: (UInt8,String) = (UInt8(49),"Crash Cymbal 1")
+    public var rithmInstruments:  [(UInt8,String)]  //  will be size 32
+    //public var midiParams: midiDefines
     public var channel :[Channel]
     
     public init(){
@@ -80,7 +85,6 @@ public struct Config{
         sigNum = 4 //UInt8(setSignatureNumerator(sig: signatureString))
         sigDen = 4 //UInt8(setSignatureDenominator(sig: signatureString))
         signatureEventInfo = [0x04,0x02,0x18,0x98]  // event for sign 4/4
-        bpmString = "120"
         bpm = 120
         resolutionString = "1/16"
         resolution = 1/16
@@ -88,26 +92,33 @@ public struct Config{
         microSecPerTick = UInt32(1000000 * 60 / UInt32(bpm))
         measureCountString = "4"
         measureCount = 4
-        loopForEverString = "0" //short for false
+        //loopForEverString = "0" //short for false
         loop = false
-        rithmPattern = 0x11111111 //nothe that ls bit is first
+        preloadedGrooves = [0x11111111,
+                           0x21212121,
+                           0x13131313,
+                           0,21412141]
+        rithmPattern = preloadedGrooves[0] //nothe that ls bit is first
         // there will be ppqn * sig denominator slots
         //for now allocate 32, may increase later
-        rithmInstruments = [UInt8]()
-        for _ in 0...31 {
-            rithmInstruments.append(34)  // 34 means no instrument
-        }
-        // to match defaulpt pattern
-        rithmInstruments[0] = 42  // closed hat  bit 0
-        rithmInstruments[4] = 42  // closed hat  bit 0
-        rithmInstruments[8] = 38  // acoustic snare   bit 8
-        rithmInstruments[12] = 42  // closed hat  bit 0
-        rithmInstruments[16] = 42  // low tom   bit 16
-        rithmInstruments[20] = 42  // low tom   bit 16
-        rithmInstruments[24] = 38  // side stick   bit 24
-        rithmInstruments[28] = 42  // low tom   bit 16
+        rithmInstruments = [(UInt8,String)]()
+        //loadInstrumentsForPattern(pattern: rithmPattern )
         
-        midiParams = midiDefines()
+        for _ in 0...31 {
+            rithmInstruments.append((0,"None"))  // 34 means no instrument
+        }
+       /*
+        // to match default pattern
+        rithmInstruments[0] = (UInt8(42),"Closed Hi Hat") // closed hat  bit 0
+        rithmInstruments[4] = (UInt8(42),"Closed Hi Hat")
+        rithmInstruments[8] = (UInt8(38),"Acustic Snare")
+        rithmInstruments[12] = (UInt8(42),"Closed Hi Hat")
+        rithmInstruments[16] = (UInt8(42),"Closed Hi Hat")
+        rithmInstruments[20] = (UInt8(42),"Closed Hi Hat")
+        rithmInstruments[24] = (UInt8(38),"Acustic Snare")
+        rithmInstruments[28] = (UInt8(42),"Closed Hi Hat")
+       */
+        //midiParams = midiDefines()
         let relatedChannels:[UInt8] = [0,0,0]
         channel = [Channel]()
         for i in (0..<16){
@@ -117,25 +128,33 @@ public struct Config{
         channel[1].type = "Opposing"
         channel[2].type = "Chord"
         channel[3].type = "Chord"
-        
-        //attempt to read defaulr config file
+        loadInstrumentsForPattern(pattern: rithmPattern) /// have to call when all is initialized
+        let status = loadConfiguration(fromURL: confFileURL)
+        if status != ""{
+            print("no default file, stick to hardcoded values")
+        }
+    }
+    
+    public mutating func loadConfiguration (fromURL:URL) -> String{
+        //attempt to read default config
+        var status :String = ""
         do{
-            print("try to read ",confFileURL)
-            let entries = try String(contentsOf:confFileURL)
+            print("try to read ",fromURL)
+            let entries = try String(contentsOf:fromURL)
             for entry in entries.split(separator:"\n"){
-                print("processing entry ",entry)
+                print("processing config file entry ",entry)
                 let param:String = String(entry.split(separator:"=")[0])
                 var valueInFile:String
                 switch(param){
-                case "sign=" :
+                case "sign" :
                     valueInFile = String(entry.split(separator:"=")[1])
                     setSign(val: valueInFile)
                     break
-                case "bpm=" :
+                case "bpm" :
                     valueInFile = String(entry.split(separator:"=")[1])
                     setBpm(val: valueInFile)
                     break
-                case "reso=" :
+                case "reso" :
                     valueInFile = String(entry.split(separator:"=")[1])
                     setReso(val: valueInFile)
                     break
@@ -145,7 +164,7 @@ public struct Config{
                     break
                 case "loop" :
                     valueInFile = String(entry.split(separator:"=")[1])
-                    setMsrc(val:valueInFile)
+                    setLoop(val:valueInFile)
                     break
                 default :
                     print("reading config found unknow param")
@@ -155,7 +174,48 @@ public struct Config{
         }
         catch{
             print("no config file, use hardcoded values")
+            status = "load settings unable read file"
         }
+        return status
+    }
+
+    public func saveConfiguration(configFileURL:URL) -> String{
+        var errorMsg = ""
+        //let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        var entries=[String]()//define empty array of strings
+        entries.append("sign="+signatureString+"\n")
+        entries.append("bpm="+String(bpm)+"\n")
+        entries.append("reso="+resolutionString+"\n")
+        entries.append("msrc="+measureCountString+"\n")
+        entries.append("loop="+String(loop)+"\n")
+        entries.append("rithmPattern="+String(rithmPattern)+"\n")
+        for i in 0...15 {
+            entries.append("instrCode"+String(i)+"="+String(rithmInstruments[i].0))
+            entries.append("instrName"+String(i)+"="+rithmInstruments[i].1)
+        }
+        for i in 0...15{
+            entries.append("ch"+String(i)+"\n")   // this is just a marker
+            entries.append("chType="+channel[i].type+"\n")
+            entries.append("scaleName="+channel[i].scale.name+"\n")
+            entries.append("scaleSharp="+channel[i].scale.sharp+"\n")
+            entries.append("scaleType="+channel[i].scale.type+"\n")
+            entries.append("chInstr="+String(channel[i].instrument)+"\n")
+            entries.append("chRange="+channel[i].instrumentRange+"\n")
+        }
+        print("in save configuration will save")
+        var dataString:String = ""
+        for entry in entries{
+            dataString += "\(entry)"
+        }
+        print (dataString)
+        do{
+            // write method overwrites as opposed to append method
+            try dataString.write(to:configFileURL,atomically: true,encoding: .utf8)
+        }catch{
+            print("error writing config file")
+            errorMsg = error.localizedDescription
+        }
+        return errorMsg
     }
 
     public mutating func setSign(val :String) ->Void{
@@ -168,7 +228,6 @@ public struct Config{
         signatureEventInfo[3] = 0x08  //  8 midi clocks per 1/32 note
     }
     public mutating func setBpm(val: String)-> Void{
-        bpmString = val
         bpm = UInt16(val) ?? 60
         microSecPerTick = UInt32(1000000 * 60 / UInt32(bpm))
     }
@@ -184,10 +243,15 @@ public struct Config{
             resolution = Double(val) ?? 1/16
             ticksPerMeasure = UInt8(val) ?? 1
         }
+        loadInstrumentsForPattern(pattern: rithmPattern) // pattern follows resolution
     }
     public mutating func setMsrc(val: String)-> Void{
         measureCountString = val
         measureCount = UInt8(val) ?? 4
+    }
+ 
+    public mutating func setLoop(val: String)-> Void{
+        loop = Bool(val) ?? false
     }
     public mutating func togleLoop()-> Void{
         if loop {
@@ -197,8 +261,8 @@ public struct Config{
             loop = true
         }
     }
-    public mutating func updateRithm(position: UInt8, newInstr :UInt8)-> Void {
-        if newInstr > 34 {
+    public mutating func updateRithm(position: UInt8, newInstr :(UInt8,String))-> Void {
+        if newInstr.0 > 0 {
             rithmPattern = setBitInWord(bitPosition: position, word32: rithmPattern)
         }
         else{
@@ -209,23 +273,38 @@ public struct Config{
     public mutating func clearRithmPattern(){
         rithmPattern = 0
         for i in (0...rithmInstruments.count - 1) {
-            rithmInstruments[i] = 0
+            rithmInstruments[i] = (0,"None")
         }
     }
     public mutating func randomRithmPattern(){
         clearRithmPattern()
         for _ in (0...ticksPerMeasure - 1) {
             let randomPosition = arc4random_uniform(UInt32(ticksPerMeasure))
-            let randomInstrumentOffset = arc4random_uniform(UInt32(midiParams.percussionInstruments.count))
-            if randomInstrumentOffset > 0{ // 0 is none
-                rithmInstruments[Int(randomPosition)] = UInt8(midiParams.percussionInstruments[    Int(randomInstrumentOffset)].0)
-                let maskValue:UInt32 = 0x01
-                rithmPattern = rithmPattern | maskValue << randomPosition
-            }
-            //update pattern:
+            rithmInstruments[Int(randomPosition)] = defaultInstrument1
+            let maskValue:UInt32 = 0x01
+            rithmPattern = rithmPattern | maskValue << randomPosition
         }
     }
-    
+    public mutating func loadInstrumentsForPattern(pattern:UInt32){
+        let beatCount = (ticksPerMeasure / sigDen) * sigNum
+        for _ in 0...beatCount-1 {
+            rithmInstruments.append((0,"None"))  // 34 means no instrument
+        }
+        var counter = 0 // will switch to default2 on 3rd beat in measure
+        for i in 0 ..< ticksPerMeasure{
+            let bitValue = pattern >> i  // least significant bit is position 1
+            if bitValue & 0x00000001 == 1{
+                counter += 1
+                if counter < 3 {
+                    rithmInstruments[Int(i)] = defaultInstrument1// closed hat  bit 0
+                }
+                else {
+                    rithmInstruments[Int(i)] = defaultInstrument2
+                    counter = 0
+                }
+            }
+        }
+    }
     public mutating func setChannelType(channelId:Int, val:String) ->Void {
         channel[channelId].type = val
     }
@@ -308,21 +387,4 @@ func makeDir(thisURL:URL,dir:String)->URL{
 }
 
 
-
-/*
- Following is to test in playground
- */
-/*
-var test = getDocDirectory()
-print (test)
-var c = Config()
-print (c.confFileURL)
-print("signature ",c.signature)
-print("num ",c.sigNum)
-print("den ",c.sigDen)
-print("bpm ",c.bpm)
-saveConfiguration(configURL: c.confFileURL, config:c)
-//deleteFile(fileURL: c.confFileURL)
-//deleteFile(fileURL: c.midiFileURL)
-*/
 
--- Configuration/midiDefines.swift
+++ Configuration/midiDefines.swift
@@ -11,7 +11,7 @@ public struct midiDefines{
     let firstNote = 0
     let lastNote = 127
     
-    public var percussionInstruments :[(Int,String)] =
+    public var percussionAllInstruments :[(UInt8,String)] =
                             [(0, "None"),
                              (35,"Acoustic Bass Drum"),
                              (36,"Bass Drum 1"),
@@ -68,6 +68,37 @@ public struct midiDefines{
                              (87,"Open Surdu")
                             ]
     
+    public var percussionInstruments  :[(UInt8,String)] =
+         [(0, "None"),
+         (35,"Acoustic Bass Drum"),
+         (36,"Bass Drum 1"),
+         (37,"Side Stick"),
+         (38,"Acustic Snare"),
+         (40,"Electric Snare"),
+         (41,"Low Floor Tom"),
+         (42,"Closed Hi Hat"),
+         (43,"Hi Floor Tom"),
+         (44,"Pedal Hi-Hat"),
+         (45,"Low Tom"),
+         (46,"Open High Hat"),
+         (47,"Low Mid Tom"),
+         (48,"Hi Mid Tom"),
+         (49,"Crash Cymbal 1"),
+         (50,"High Tom"),
+         (51,"Ride Cymbal 1"),
+         (52,"Chinese Cymbal"),
+         (53,"Ride Bell"),
+         (54,"Tabourine"),
+         (55,"Splash Cymbal"),
+         (56,"Cow Bell"),
+         (57,"Crash Cymbal 2"),
+         (59,"Ride Cymbal 2"),
+         (65,"High Timbale"),
+         (66,"Low Timbale"),
+         (80,"Triangle Mute"),
+         (81,"Triange Open"),
+
+    ]
     let pianoStart = 0
     let percussionStart = 9
     let organStart = 17
